13db7396b61a11245838abbf7c699faaa29544c6
seoyy <seoyy@175.198.126.133>
2019-09-12 12:25:27 +0900

    비밀번호 변경 검증

--- project444/src/userControl/PwUpdateEvt.java
+++ project444/src/userControl/PwUpdateEvt.java
@@ -24,19 +24,22 @@ public class PwUpdateEvt implements ActionListener {
 		String mfPw=String.valueOf(cMfPw);
 		String remfPw=String.valueOf(cRemfPw);
 		if(uDAO.selectPw(curPw).isEmpty()) {
-			JOptionPane.showMessageDialog(pu,"입력하신 현재 비밀번호가 올바르지 않습니다.");
+			JOptionPane.showMessageDialog(pu,"현재 비밀번호를 입력해주세요.");
 		}else {
 			if(!mfPw.equals(remfPw)) {
 				JOptionPane.showMessageDialog(pu, "변경하실 비밀번호와 비밀번호 확인이 올바르지 않습니다.");
 			}else {
-				boolean flag = uDAO.updatePw(pu.getId(), mfPw);
-				if(flag==false){
-					JOptionPane.showMessageDialog(pu,"변경하실 비밀번호를 입력해주세요.");
-					
-				}else {
-					JOptionPane.showMessageDialog(pu,"비밀번호 변경이 올바르게 되었습니다.");
-					PwUpdateClose();
-				}
+				if(mfPw.equals(curPw)) {
+					JOptionPane.showMessageDialog(pu, "현재 비밀번호와 입력하신 변경할 비밀번호가 일치합니다.");
+					}else {
+						boolean flag = uDAO.updatePw(pu.getId(), mfPw);
+							if(flag==false){
+								JOptionPane.showMessageDialog(pu,"변경하실 비밀번호를 입력해주세요.");
+							}else {
+								JOptionPane.showMessageDialog(pu,"비밀번호 변경이 올바르게 되었습니다.");
+									PwUpdateClose();
+						}//end else
+				}//end else
 			}//end else
 			
 		}//end else
@@ -52,7 +55,7 @@ public class PwUpdateEvt implements ActionListener {
 		if(ae.getSource()==pu.getJbtCancle()) {
 			PwUpdateClose();
 		}//end if
-		if(ae.getSource()==pu.getJbtOk()) {
+		if(ae.getSource()==pu.getJbtOk()||ae.getSource()==pu.getJpfReUpdatePw()) {
 			try {
 				modifyPw();
 			} catch (SQLException e) {
