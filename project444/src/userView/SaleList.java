8205884659a3e54dede8e35dd88b713992320faf
seoyy <seoyy@175.198.126.133>
2019-09-12 12:16:25 +0900

    불필요 패키지 정리

--- project444/src/userView/SaleList.java
+++ project444/src/userView/SaleList.java
@@ -128,15 +128,15 @@ public class SaleList extends JDialog {
 		jtComplete.getTableHeader().setReorderingAllowed(false);//테이블 컬럼 위치 변경 금지
 		
 		jtComplete.getColumnModel().getColumn(0).setPreferredWidth(120);
-		jtComplete.getColumnModel().getColumn(0).setResizable(false);;//테이블 컬럼 사이즈 변경 금지
+		jtComplete.getColumnModel().getColumn(0).setResizable(false);//테이블 컬럼 사이즈 변경 금지
 		jtComplete.getColumnModel().getColumn(1).setPreferredWidth(120);
-		jtComplete.getColumnModel().getColumn(1).setResizable(false);;
+		jtComplete.getColumnModel().getColumn(1).setResizable(false);
 		jtComplete.getColumnModel().getColumn(2).setPreferredWidth(80);
-		jtComplete.getColumnModel().getColumn(2).setResizable(false);;
+		jtComplete.getColumnModel().getColumn(2).setResizable(false);
 		jtComplete.getColumnModel().getColumn(3).setPreferredWidth(80);
-		jtComplete.getColumnModel().getColumn(3).setResizable(false);;
+		jtComplete.getColumnModel().getColumn(3).setResizable(false);
 		jtComplete.getColumnModel().getColumn(4).setPreferredWidth(80);
-		jtComplete.getColumnModel().getColumn(4).setResizable(false);;
+		jtComplete.getColumnModel().getColumn(4).setResizable(false);
 		
 		jtComplete.setRowHeight(100);
 		
