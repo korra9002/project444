3b36f5f9b6b3e8db9b45890d8f974737f1cee168
owner <owner@DESKTOP-D81PLRF>
2019-09-16 15:09:06 +0900

    채팅등등 수정중

--- project444/src/userControl/MarketDetailBuyerEvt.java
+++ project444/src/userControl/MarketDetailBuyerEvt.java
@@ -4,26 +4,59 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import com.sun.webkit.ContextMenu.ShowContext;
 
 import chatTest.ChattingView;
+import userDAO.UserDAO;
+import userRun.RunMarketMain;
 import userView.MarketDetailBuyer;
 import userView.MarketMain;
 
 public class MarketDetailBuyerEvt extends MouseAdapter implements ActionListener{
 	private MarketDetailBuyer mdb;
 	private MarketMain mm;
+	String id;
 	
 	public MarketDetailBuyerEvt (MarketMain mm, MarketDetailBuyer mdb) {
 		this.mdb=mdb;
 		this.mm=mm;
+		id = RunMarketMain.userId;
 	}//MarketDetailBuyerEvt
 
-
+	public void dealStart() {
+		UserDAO uDAO = UserDAO.getInstance();
+		try {
+		int result= uDAO.addDeal(mdb.getProductCode(), mdb.getId());
+		
+	switch (result) {
+	case -1:
+		JOptionPane.showMessageDialog(mdb, "이미 거래중 입니다.");
+		break;
+	case 1:
+		
+		break;
+	default:
+		JOptionPane.showMessageDialog(mdb, "채팅 실패");
+		break;
+	}
+		
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 	@Override
 	public void actionPerformed(ActionEvent ae) {
 		if(ae.getSource()==mdb.getJbtChat()) {
 //			new ChattingView(mm);
-		
+		dealStart();
 		}
 	}//actionPerformed
 
