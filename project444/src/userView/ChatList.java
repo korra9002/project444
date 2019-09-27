5ea70f31930e416a592c044ea0b034e61883b6aa
sist <sist@DESKTOP-3L0A3H5>
2019-09-27 15:52:56 +0900

    ++ 조장님 채팅뷰 테이블2개 만듦

--- project444/src/userView/ChatList.java
+++ project444/src/userView/ChatList.java
@@ -6,6 +6,7 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JRadioButton;
@@ -24,22 +25,23 @@ import userControl.ChatListEvt;
 public class ChatList extends JPanel {
 
 	private JButton jbRefresh, jbBuy, jbSell;
+	private JLabel jlComplete;
 
-	private DefaultTableModel dtmProductList;
-	private JTable jtProductList;
+	private DefaultTableModel dtmProductList, dtmProductList2;
+	private JTable jtProductList,jtProductList2;
 	
 	private ChatListEvt cle;
 
 	public ChatList() {
 
-		//////////////////////// 채팅 화면 //////////////////////////////
+		/////////////////////////////////////////// 채팅 화면 /////////////////////////////////////////////
 
 //		홈, 글쓰기, 채팅, MyPage
 
 		// JButton
 		jbRefresh = new JButton("새로고침");
 		jbBuy = new JButton("구매중");
-		jbSell = new JButton("판매중");
+		jbSell = new JButton("판매중");		   
 		// JTable
 
 		String[] productColumn = { "제품명", "ID", "지역", "시간", "마지막 대화 내용","거래코드" };
@@ -56,10 +58,13 @@ public class ChatList extends JPanel {
 			
 			
 		};
+
 		
 		jtProductList = new JTable(dtmProductList);
 
+
 		JScrollPane jspProductList = new JScrollPane(jtProductList);
+
 		// 리스트 크기, 이동, 편집 불가능하게 설정
 
 		jtProductList.setRowHeight(30);
@@ -81,20 +86,70 @@ public class ChatList extends JPanel {
 		tcm.getColumn(3).setCellRenderer(dtcrCenter);
 		tcm.getColumn(5).setCellRenderer(dtcrCenter);
 		/////////////////////////테이블 가운데 정렬////////////////////////////////
-//		setResizable(false);
+		
+		
+		
+		// +++++++++++++++++++++++++++++++++++++++++++++++++++ 2번째 테이블 ++++++++++++++++++++++++++++++++++++++++++++++++ //
+		
+		//Jlabel
+		jlComplete =new JLabel("라벨 여깄다리~");
+		
+		//JTable
+		dtmProductList2 = new DefaultTableModel(productColumn, 6) {		
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}					
+		};
+		
+		jtProductList2 = new JTable(dtmProductList2);
+		JScrollPane jspProductList2 = new JScrollPane(jtProductList2);
+		
+		// 리스트 크기, 이동, 편집 불가능하게 설정
+		jtProductList2.setRowHeight(30);
+		jtProductList2.getTableHeader().setReorderingAllowed(false);
+		
+		jtProductList2.getColumnModel().getColumn(0).setPreferredWidth(100);
+		jtProductList2.getColumnModel().getColumn(1).setPreferredWidth(10);
+		jtProductList2.getColumnModel().getColumn(2).setPreferredWidth(10);
+		jtProductList2.getColumnModel().getColumn(3).setPreferredWidth(90);
+		jtProductList2.getColumnModel().getColumn(4).setPreferredWidth(220);
+		jtProductList2.getColumnModel().getColumn(5).setPreferredWidth(10);
+		/////////////////////////테이블 가운데 정렬////////////////////////////////
+		DefaultTableCellRenderer dtcrCenter2 = new DefaultTableCellRenderer();
+		dtcrCenter2.setHorizontalAlignment(SwingConstants.CENTER);
+		TableColumnModel tcm2 = jtProductList.getColumnModel();
+		tcm2.getColumn(0).setCellRenderer(dtcrCenter);
+		tcm2.getColumn(1).setCellRenderer(dtcrCenter);
+		tcm2.getColumn(2).setCellRenderer(dtcrCenter);
+		tcm2.getColumn(3).setCellRenderer(dtcrCenter);
+		tcm2.getColumn(5).setCellRenderer(dtcrCenter);
+		/////////////////////////테이블 가운데 정렬////////////////////////////////
+
+		
+		// +++++++++++++++++++++++++++++++++++++++++++++++++++ 2번째 테이블 ++++++++++++++++++++++++++++++++++++++++++++++++ //
+		
+				
 
 		// 컴포넌트 배치
 		setLayout(null);
 
-		jspProductList.setBounds(30, 80, 750, 700);
+		jspProductList.setBounds(30, 80, 750, 300);
 		jbRefresh.setBounds(680, 20, 100,50);
 		jbBuy.setBounds(30, 20, 90, 50);
 		jbSell.setBounds(140, 20, 90, 50);
-
+		
+		
+		jspProductList2.setBounds(30, 450, 750, 300);
+		jlComplete.setBounds(30, 410, 120, 50);
+		
 		add(jspProductList);
 		add(jbRefresh);
 		add(jbBuy);
 		add(jbSell);
+		add(jlComplete);
+		
+		add(jspProductList2);
 
 //		setBounds(100, 100,  600, 450);
 		setBackground(new Color(0xf6f2ef));
@@ -102,6 +157,9 @@ public class ChatList extends JPanel {
 		jbBuy.setBackground(new Color(0xFFCC66));
 		jbSell.setBackground(new Color(0xFFCC66));
 		jbRefresh.setBackground(new Color(0xFFCC66));
+		jtProductList2.getTableHeader().setBackground(new Color(0xFFCC66));
+
+		
 		
 		setVisible(true);
 		cle = new ChatListEvt(this);
@@ -109,16 +167,15 @@ public class ChatList extends JPanel {
 		jbBuy.addActionListener(cle);
 		jbSell.addActionListener(cle);
 		jtProductList.addMouseListener(cle);
+		
+
+		jtProductList2.addMouseListener(cle);
 
 		jspProductList.getVerticalScrollBar().setUI(new ScrollBarUI());
+		jspProductList2.getVerticalScrollBar().setUI(new ScrollBarUI());
 //		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}// Chat
-
-	public ChatListEvt getCle() {
-		return cle;
-	}
-
 
 	public JButton getJbRefresh() {
 		return jbRefresh;
@@ -132,12 +189,33 @@ public class ChatList extends JPanel {
 		return jbSell;
 	}
 
+	public JLabel getJlComplete() {
+		return jlComplete;
+	}
+
 	public DefaultTableModel getDtmProductList() {
 		return dtmProductList;
+	}
+
+	public DefaultTableModel getDtmProductList2() {
+		return dtmProductList2;
 	}
 
 	public JTable getJtProductList() {
 		return jtProductList;
 	}
+
+	public JTable getJtProductList2() {
+		return jtProductList2;
+	}
+
+	public ChatListEvt getCle() {
+		return cle;
+	}
+
+
+	
+
+
 
 }// class
