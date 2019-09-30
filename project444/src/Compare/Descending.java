437a9e3ff9480705ba1ea3ef417ccab898d03f78
owner <owner@DESKTOP-D81PLRF>
2019-09-30 10:51:21 +0900

    채팅 정렬

--- project444/src/Compare/Descending.java
+++ project444/src/Compare/Descending.java
@@ -12,10 +12,17 @@ public class Descending implements Comparator{
 			ChatListVO clVO1 = (ChatListVO)o1;
 			ChatListVO clVO2 = (ChatListVO)o2;
 //			System.out.println("컴페어"+(clVO1.getTime().isEmpty() ? "공백" : (clVO1.getTime().compareTo(clVO2.getTime())*-1)));
-			if (clVO1.getTime().isEmpty() || clVO2.getTime().isEmpty()) {
+			if (clVO1.getTime().isEmpty() && clVO2.getTime().isEmpty()) {
+				return 0;
+			}
+			if (clVO1.getTime().isEmpty()) {
+				return -1;
+			}
+			if (clVO2.getTime().isEmpty()) {
 				return 1;
 			}
-			return clVO1.getTime().isEmpty() ? 1 : (clVO1.getTime().compareTo(clVO2.getTime())*-1);
+			
+			return  (clVO1.getTime().compareTo(clVO2.getTime())*-1);
 			
 		}
 		
